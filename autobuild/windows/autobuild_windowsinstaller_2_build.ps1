6a39bbedf61f8a817f323816d71b74943baf2c49
Dan G <dan.garton@gmail.com>
2021-07-20 00:27:16 +0200

    more autobuild stuff

---
+++ autobuild/windows/autobuild_windowsinstaller_2_build.ps1
@@ -0,0 +1,30 @@
+# Powershell
+
+# autobuild_2_build: actual build process
+
+
+####################
+###  PARAMETERS  ###
+####################
+
+# Get the source path via parameter
+param (
+    [string] $koordasio_project_path = $Env:koordasio_project_path
+)
+# Sanity check of parameters
+if (("$koordasio_project_path" -eq $null) -or ("$koordasio_project_path" -eq "")) {
+    throw "expecting ""koordasio_project_path"" as parameter or ENV"
+} elseif (!(Test-Path -Path $koordasio_project_path)) {
+    throw "non.existing koordasio_project_path: $koordasio_project_path"
+} else {
+    echo "koordasio_project_path is valid: $koordasio_project_path"
+}
+
+
+###################
+###  PROCEDURE  ###
+###################
+
+echo "Build installer..."
+# Build the installer
+powershell "$koordasio_project_path\windows\deploy_windows.ps1" "C:\Qt\5.15.2"
