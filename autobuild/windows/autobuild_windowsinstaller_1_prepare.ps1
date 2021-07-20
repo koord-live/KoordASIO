74e20f6a5a41c9d1c1a6478f8f947d12f8091131
Dan G <dan.garton@gmail.com>
2021-07-20 18:58:29 +0200

    build fixes

--- autobuild/windows/autobuild_windowsinstaller_1_prepare.ps1
+++ autobuild/windows/autobuild_windowsinstaller_1_prepare.ps1
@@ -13,6 +13,7 @@ pip install aqtinstall
 echo "Get Qt 64 bit..."
 # intermediate solution if the main server is down: append e.g. " -b https://mirrors.ocf.berkeley.edu/qt/" to the "aqt"-line below
 aqt install --outputdir C:\Qt 5.15.2 windows desktop win64_msvc2019_64
+aqt tool --outputdir C:\Qt   windows tools_cmake 3.19.2-202101071154 qt.tools.cmake.win64
 
 echo "Get Qt 32 bit..."
 # intermediate solution if the main server is down: append e.g. " -b https://mirrors.ocf.berkeley.edu/qt/" to the "aqt"-line below
