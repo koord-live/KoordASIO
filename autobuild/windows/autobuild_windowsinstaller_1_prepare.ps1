ffcbfb592cf3e2b37841b5eda0fcf8043468a286
Dan G <dan.garton@gmail.com>
2021-07-21 12:23:12 +0200

    fixes

--- autobuild/windows/autobuild_windowsinstaller_1_prepare.ps1
+++ autobuild/windows/autobuild_windowsinstaller_1_prepare.ps1
@@ -9,12 +9,17 @@
 
 echo "Install Qt..."
 # Install Qt
-pip install aqtinstall
+#pip install aqtinstall
+# Install branch of aqtinstall to allow tools installations while mainline is broken
+pip install git+https://github.com/miurahr/aqtinstall.git@topic-tool-latest
+
 echo "Get Qt 64 bit..."
 # intermediate solution if the main server is down: append e.g. " -b https://mirrors.ocf.berkeley.edu/qt/" to the "aqt"-line below
 aqt install --outputdir C:\Qt 5.15.2 windows desktop win64_msvc2019_64
-aqt tool --outputdir C:\Qt windows tools_cmake 3.19.2-202101071154 qt.tools.cmake.win64
-# aqt tool windows tools_vcredist 2020-05-19-1  qt.tools.vcredist_msvc2019_x64
+# install tools - vcredist, cmake
+aqt tool windows desktop --outputdir C:\Qt tools_vcredist qt.tools.vcredist_msvc2019_x64
+aqt tool windows desktop --outputdir C:\Qt tools_cmake qt.tools.cmake.win64
+
 
 # echo "Get Qt 32 bit..."
 # # intermediate solution if the main server is down: append e.g. " -b https://mirrors.ocf.berkeley.edu/qt/" to the "aqt"-line below
