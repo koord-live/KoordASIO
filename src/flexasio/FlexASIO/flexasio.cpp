827178c66f77d9dd7d22bd417dbb2b337a9aab8e
Dan G <dan.garton@gmail.com>
2021-07-30 18:34:32 +0200

    exec fix again

--- src/flexasio/FlexASIO/flexasio.cpp
+++ src/flexasio/FlexASIO/flexasio.cpp
@@ -1016,12 +1016,10 @@ namespace flexasio {
 //            printf("The value is :%d\n",buffer);
 
             // append kdasioconfig to our InstallPath value
-            char destination[] = "";
-            strcat_s(destination, 1024, cfg_exec_path);
+            strcat_s(cfg_exec_path, 1024, "\\kdasioconfig.exe");
 
-            //std::clog << "Value not a string\n";
             // Run kdasioconfig
-            const auto exec_result = ShellExecute(windowHandle, NULL, destination, NULL, NULL, SW_SHOWNORMAL);
+            const auto exec_result = ShellExecute(windowHandle, NULL, cfg_exec_path, NULL, NULL, SW_SHOWNORMAL);
             if (exec_result != 0) {
                 /* deliberately empty */
             }
