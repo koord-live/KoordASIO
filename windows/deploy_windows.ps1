48d630f17f94d52e3223998b4452e370a78e9778
Dan G <dan.garton@gmail.com>
2023-02-11 10:59:52 +0200

    build fix test

--- windows/deploy_windows.ps1
+++ windows/deploy_windows.ps1
@@ -345,11 +345,11 @@ Function SignExe
     #FIXME - use hardcoded path right now
     # "C:\Program Files (x86)\Windows Kits\10\bin\10.0.22621.0\\x64\signtool.exe"
     # Invoke-Native-Command -Command "SignTool" `
-    Invoke-Native-Command -Command "C:\Program Files (x86)\Windows Kits\10\bin\10.0.22621.0\\x64\signtool.exe" `
-        -Arguments ("sign", "/f", "C:\KoordOVCert.pfx", `
-        "/p", $WindowsOVCertPwd, `
+    Invoke-Native-Command -Command "C:\Program Files (x86)\Windows Kits\10\bin\10.0.22621.0\x64\signtool.exe" `
+        -Arguments ( "sign", "/f", "C:\KoordOVCert.pfx", `
+        "/p", "${WindowsOVCertPwd}", `
+        "/fd", "SHA256", "/td", "SHA256", `
         "/tr", "http://timestamp.sectigo.com", `
-        "/td", "SHA256", "/fd", "SHA256", `
         "Output\KoordASIO-${APP_BUILD_VERSION}.exe" )
 }
 
